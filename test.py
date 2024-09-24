24a194ccf3bb502e0b4540711bf0a18a6711cddc
ryzen's tech! <158189251+ryzenstechdev@users.noreply.github.com>
2024-09-24 07:16:49 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+# Mi faccio delle seghe!!!!!!!!!!!!
+#
+# by ryzenstech
+
+Question = input("What's 9+10?\nAnswer: ")
+
+if Question == "21":
+    print("Correct!")
+else:
+    print("KILL YOURSELF!!!")
